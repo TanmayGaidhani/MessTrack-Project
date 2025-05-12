8394e8e88fea539cc27889c503d4b41455fbc2d9
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2025-05-12 11:02:37 +0530

    Delete userloginpage.js

--- userloginpage.js
+++
@@ -1,8 +0,0 @@
-const inputs = document.querySelectorAll(".otp-block input");
-inputs.forEach((input, index) => {
-input.addEventListener("input", () => {
-    if (input.value.length === 1 && index < inputs.length - 1) {
-        inputs[index + 1].focus();
-    }
-    });
-});
